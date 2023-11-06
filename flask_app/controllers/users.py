bc36e1c49ce1e540d6498480d198a1b6ed177ffa
Casey D'Eon <cdeon731@gmail.com>
2023-11-06 13:15:23 -0500

    first_commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,13 @@
+from flask_app import app
+from flask import render_template, request, redirect, session
+from flask_app.models import user
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/1')
+def next_page():
+    return render_template('dashboard2.html')
---
+++ flask_app/models/user.py
@@ -0,0 +1,8 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask_bcrypt import Bcrypt
+from flask_app import app
+bcrypt = Bcrypt(app)
+# from flask_app.models import #models
